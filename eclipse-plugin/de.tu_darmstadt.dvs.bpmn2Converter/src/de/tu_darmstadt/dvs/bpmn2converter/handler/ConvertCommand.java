e75e3d832732d4f7b5135707a6c10dc9b38dbaeb
Mr Dang <dangquochien@gmail.com>
2012-05-29 02:04:24 +0200

    reognizing

--- eclipse-plugin/de.tu_darmstadt.dvs.bpmn2Converter/src/de/tu_darmstadt/dvs/bpmn2converter/handler/ConvertCommand.java
+++ eclipse-plugin/de.tu_darmstadt.dvs.bpmn2Converter/src/de/tu_darmstadt/dvs/bpmn2converter/handler/ConvertCommand.java
@@ -1,78 +1,86 @@
 package de.tu_darmstadt.dvs.bpmn2converter.handler;
 
-import java.io.BufferedWriter;
+import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.Reader;
-import java.io.StringBufferInputStream;
 import java.io.UnsupportedEncodingException;
+
+//import org.jdom2.
 
 import org.eclipse.core.commands.AbstractHandler;
 import org.eclipse.core.commands.ExecutionEvent;
 import org.eclipse.core.commands.ExecutionException;
-import org.eclipse.core.resources.IResource;
 import org.eclipse.core.runtime.CoreException;
-import org.eclipse.core.runtime.QualifiedName;
-import org.eclipse.jdt.core.Flags;
-import org.eclipse.jdt.core.ICompilationUnit;
-//import org.eclipse.jdt.core.I
-import org.eclipse.jdt.core.IMethod;
-import org.eclipse.jdt.core.IType;
-import org.eclipse.jdt.core.JavaModelException;
 import org.eclipse.jface.dialogs.MessageDialog;
 import org.eclipse.jface.viewers.IStructuredSelection;
-import org.eclipse.swt.widgets.DirectoryDialog;
 import org.eclipse.ui.handlers.HandlerUtil;
 import org.eclipse.core.resources.IFile;
-import org.eclipse.core.internal.resources.File;
+import org.jdom2.Attribute;
+import org.jdom2.Element;
+
 public class ConvertCommand extends AbstractHandler {
-	private QualifiedName path = new QualifiedName("html", "path");
 
 	@Override
 	public Object execute(ExecutionEvent event) throws ExecutionException {
-
 		IStructuredSelection selection = (IStructuredSelection) HandlerUtil
 				.getActiveMenuSelection(event);
-
-		String directory = "";
+		
 		Object firstElement = selection.getFirstElement();
-		System.out.println("debug:\t" + firstElement.getClass());
+		System.out.println(selection.size() + " elements");
+		if(selection.size() != 1) {
+			//TODO : warning, just choose 1 file.
+			return null;
+		}
 		if (firstElement instanceof IFile) {
 			IFile file = (IFile) firstElement;
 			String extension =file.getFileExtension();
+			System.out.println("location:" +file.getLocation());
+			String oFileLocation = file.getLocation().toOSString();
 			System.out.println(extension);
+			
+			String nfileLocation = oFileLocation + "\n";
+			nfileLocation = nfileLocation.replace(extension+"\n", "ukuf");
 			if (extension.equals("bpmn") || extension.equals("bpmn2")) {
-				//TODO:
-				System.out.println("converting..");
+				//TODO: check, convert bpmn code to Ukuflow byte code
 				
-				InputStream input = null;
+				System.out.println("checking..");
+				System.out.println("converting..");			
+				BPMN2XMLParser parser = new BPMN2XMLParser();
+				Element e = parser.loadFile(oFileLocation);
+				
+				printElement(e, "");
+				//new Converter(input,null).execute();
+				//Converter.execute(input);
+				//new Converter(input, "").execute();
+				/*
+				File f = new File(oFileLocation);				
+				FileWriter fwrite = null;
 				try {
-					input = file.getContents();
-					readFile(input);
-				} catch (CoreException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
+					fwrite = new FileWriter(f);
+					fwrite.write(content);
+					fwrite.flush();
+					fwrite.close();
+				} catch (IOException ex) {
+					ex.printStackTrace();
 				}
-				
+				*/
 			} else {
-				MessageDialog.openInformation(
-						HandlerUtil.getActiveShell(event), "Information",
-						"Please select a BPMN or BPMN2 source file");
+				MessageDialog.openInformation(HandlerUtil.getActiveShell(event), "Information",	"Please select a BPMN or BPMN2 source file");
 			}
 
 		} else {
-			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
-					"Information", "Please select a BPMN or BPMN2 source file");
+			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),"Information", "Please select a BPMN or BPMN2 source file");
 		}
-		
+		/*
 		if (firstElement instanceof ICompilationUnit) {
 			ICompilationUnit cu = (ICompilationUnit) firstElement;
 			IResource res = cu.getResource();
 			boolean newDirectory = true;
 			directory = getPersistentProperty(res, path);
-
+			System.out.println("directory: " + directory);
 			if (directory != null && directory.length() > 0) {
 				newDirectory = !(MessageDialog.openQuestion(
 						HandlerUtil.getActiveShell(event), "Question",
@@ -93,10 +101,10 @@ public class ConvertCommand extends AbstractHandler {
 		} else {
 			MessageDialog.openInformation(HandlerUtil.getActiveShell(event),
 					"Information", "Please select a Java source file");
-		}
+		}*/
 		return null;
 	}
-
+/*
 	protected String getPersistentProperty(IResource res, QualifiedName qn) {
 		try {
 			return res.getPersistentProperty(qn);
@@ -104,41 +112,11 @@ public class ConvertCommand extends AbstractHandler {
 			return "";
 		}
 	}
-
+*/
 	// TODO: Include this in the HTML output
 
-	private void analyze(ICompilationUnit cu) {
-		// Cool JDT allows you to analyze the code easily
-		// I don't see really a use case here but I just wanted to do this here
-		// as I consider this as cool and
-		// what to have a place where I can store the data
-		try {
-
-			IType type = null;
-			IType[] allTypes;
-			allTypes = cu.getAllTypes();
-
-			/**
-			 * Search the public class
-			 */
-
-			for (int t = 0; t < allTypes.length; t++) {
-				if (Flags.isPublic((allTypes[t].getFlags()))) {
-					type = allTypes[t];
-					break;
-				}
-			}
-			if (type != null) {
-				String classname = type.getFullyQualifiedName();
-				IMethod[] methods = type.getMethods();
-			}
-
-		} catch (JavaModelException e) {
-			e.printStackTrace();
-		}
-
-	}
-
+	
+/*
 	protected void setPersistentProperty(IResource res, QualifiedName qn,
 			String value) {
 		try {
@@ -147,38 +125,8 @@ public class ConvertCommand extends AbstractHandler {
 			e.printStackTrace();
 		}
 	}
-
-	private void write(String dir, ICompilationUnit cu) {
-		try {
-			cu.getCorrespondingResource().getName();
-			String test = cu.getCorrespondingResource().getName();
-			// Need
-			String[] name = test.split("\\.");
-			System.out.println("debug:\t" + test);
-			System.out.println("debug:\t" + name.length);
-			String htmlFile = dir + "\\" + name[0] + ".html";
-
-			System.out.println("debug:\t" + htmlFile);
-			FileWriter output = new FileWriter(htmlFile);
-			BufferedWriter writer = new BufferedWriter(output);
-			writer.write("<html>");
-			writer.write("<head>");
-			writer.write("</head>");
-			writer.write("<body>");
-			writer.write("<pre>");
-			System.out.println("debug:\t"+cu.getSource());
-			writer.write(cu.getSource());
-			writer.write("</pre>");
-			writer.write("</body>");
-			writer.write("</html>");
-			writer.flush();
-		} catch (JavaModelException e) {
-		} catch (IOException e) {
-			e.printStackTrace();
-		}
-
-	}
-	private void readFile(InputStream is){
+*/
+	private String readFile(InputStream is){
 		final char[] buffer = new char[0x10000];
 		StringBuilder out = new StringBuilder();
 		Reader in = null;
@@ -203,6 +151,19 @@ public class ConvertCommand extends AbstractHandler {
 		} while (read>=0);
 		
 		String result = out.toString();
-		System.out.println(result);
+		return result;
+	}
+	private void printElement(Element e, String prefix){
+		System.out.print(prefix);
+		
+		System.out.print(e.getName()+":"+e.getTextTrim());
+		System.out.print(" -> (");
+		for(Attribute attrb : e.getAttributes()){
+			System.out.print(attrb.getName() + ":" + attrb.getValue() + "; ");
+		}
+		System.out.println(")");
+		for(Element child : e.getChildren()){
+			printElement(child, prefix + "   ");
+		}
 	}
 }
