75b719a6993603384c4b5e32e8c227d5175277d0
Hien Quoc Dang <dangquochien@gmail.com>
2012-07-05 02:18:08 +0200

    update workflowcode

---
+++ eclipse-plugin/de.tu_darmstadt.dvs.bpmn2Converter/src/de/tu_darmstadt/dvs/bpmn2converter/constant/EventTypes.java
@@ -0,0 +1,26 @@
+package de.tu_darmstadt.dvs.bpmn2converter.constant;
+
+public class EventTypes {
+	/* event-types.h */
+	public static final int IMMEDIATE_E_GEN = 0;
+	public static final int ABSOLUTE_E_GEN = 1;
+	public static final int OFFSET_E_GEN = 2;
+	public static final int RELATIVE_E_GEN = 3;
+	public static final int PERIODIC_E_GEN = 4;
+	public static final int PATTERNED_E_GEN = 5;
+	public static final int DISTRIBUTED_E_GEN = 6;
+	public static final int SIMPLE_FILTER = 7;
+	public static final int AND_COMPOSITION_FILTER = 8;
+	public static final int OR_COMPOSITION_FILTER = 9;
+	public static final int NOT_COMPOSITION_FILTER = 10;
+	public static final int SEQUENCE_COMPOSITION_FILTER = 11;
+	public static final int MIN_COMPOSITION_FILTER = 12;
+	public static final int MAX_COMPOSITION_FILTER = 13;
+	public static final int COUNT_COMPOSITION_FILTER = 14;
+	public static final int SUM_COMPOSITION_FILTER = 15;
+	public static final int AVG_COMPOSITION_FILTER = 16;
+	public static final int STDEV_COMPOSITION_FILTER = 17;
+	public static final int INCREASE_FILTER = 18;
+	public static final int DECREASE_FILTER = 19;
+	public static final int REMAIN_FILTER = 20;
+}
